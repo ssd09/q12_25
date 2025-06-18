10d63887857ba1a8bab3549709029016e537bdad
shivsharad.dhiman <43615987+ssd09@users.noreply.github.com>
2025-06-18 09:33:26 +0530

    Update save-selections.js

--- pages/api/save-selections.js
+++ pages/api/save-selections.js
@@ -5,13 +5,7 @@ export default async function handler(req, res) {
     const googleResponse = await fetch("https://script.google.com/macros/s/AKfycbyMbBzq_r3PE2rLhAa4zfLKnEe_ou82JpZQ6rm25mDIr55aJvH7RNejYk7gzbPKb48G/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        "q1": { "answer": "No", "priority": 2 },
-        q2: { answer: "No", priority: 1 },
-        q3: { answer: "No", priority: 3 },
-        q4: { answer: "No", priority: 4 },
-        q5: { answer: "No", priority: 5 }
-      }),
+      body: req.body,
     })
 
     const result = await googleResponse.text();
