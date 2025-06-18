e4b6b876fc9272c94537835396aac1e7d1f0d69b
shivsharad.dhiman <43615987+ssd09@users.noreply.github.com>
2025-06-18 16:39:35 +0530

    Update save-selections.js

--- pages/api/save-selections.js
+++ pages/api/save-selections.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).end("Only POST allowed");
 
   try {
-    const googleResponse = await fetch("https://script.google.com/macros/s/AKfycbyMbBzq_r3PE2rLhAa4zfLKnEe_ou82JpZQ6rm25mDIr55aJvH7RNejYk7gzbPKb48G/exec", {
+    const googleResponse = await fetch("https://script.google.com/macros/s/AKfycbzBzt2HLCh9ir2ceDLFNSVsKhzIJIUcy2EXMuZ7Om05s_L-vVlYViRgKXRkaSRn9PFK/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(req.body),
